6be3c36af5bd25377646f505dd075f51a5ae6f52
Richardyuan <richardyuan44918@gmail.com>
2024-11-15 16:30:00 +0800

    BDSE36_lesson1

--- Module3/3-4_input.py
+++ Module3/3-4_input.py
@@ -1,9 +1,12 @@
 # input() 函數 讓使用者在終端機輸入資料
 
 # 取得使用者輸入的資料
+data = input('輸入文字 按下Enter:')
 
-
-
+print(data)
 # 將使用者輸入強制轉型成 int
+num = int(input('請輸入數字'))
+print(type(num))
+print(num)
 
 
--- Module3/practice.py
+++ Module3/practice.py
@@ -5,6 +5,20 @@
 # 數字太小 要提示「請輸入更大的數字」
 # 使用者猜對要回傳「恭喜中獎」
 
+secret_num = 50
+while True:
+    num = int(input("請輸入1~100:"))
+    if num > 100 :
+        print('超出範圍請重新輸入')
+        continue
+    if num == secret_num:
+        print('恭喜中獎')
+        break
+    if num > 50 :
+        print('請輸入更小的數字')
+    else:
+        print('請輸入更大的數字')
+
 
 
 
